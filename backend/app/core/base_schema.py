3965f6c01a016198048c711a51c2f534b6388628
Vidon <tung.ma@expertsystems.com.hk>
2026-06-17 21:14:57 +0800

    minor update

--- backend/app/core/base_schema.py
+++ backend/app/core/base_schema.py
@@ -2,9 +2,22 @@ from datetime import datetime
 from uuid import UUID
 from pydantic import BaseModel, ConfigDict, Field
 
+from pydantic import model_validator
+
 class BaseSchema(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     id: UUID | None = Field(default=None, description="Primary Key")
     created_at: datetime = Field(..., description="Create record datetime")
     updated_at: datetime = Field(..., description="Update record datetime")
+
+
+    @model_validator(mode="after")
+    def move_fields_to_end(self) -> "BaseSchema":
+        d = self.__dict__
+        sort_fields = ["created_at", "updated_at"]
+        for field in sort_fields:
+            if field in d:
+                val = d.pop(field)
+                d[field] = val
+        return self
