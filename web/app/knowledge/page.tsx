b16c7225e33ed05f90e3758e5af1684c67f4f6f1
Vidon <tung.ma@expertsystems.com.hk>
2026-09-12 10:46:36 +0800

    minor update

--- web/app/knowledge/page.tsx
+++ web/app/knowledge/page.tsx
@@ -1,13 +1,6 @@
 import { DatasetDataTable } from "@/components/knowledge/data-table"
-import { getQueryClient } from "@/lib/react-query"
-import { datasetListOptions } from "@/services/dataset.service"
-import { dehydrate, HydrationBoundary, noop } from "@tanstack/react-query"
 
-interface PageProps {
-  searchParams: Promise<Record<string, string | number | boolean>>
-}
-
-export default async function Page({ searchParams }: PageProps) {
+export default async function Page() {
   return (
     <div className="p-4">
       <DatasetDataTable />
